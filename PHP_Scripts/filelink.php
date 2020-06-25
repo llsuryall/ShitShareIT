fb5d8f0bff2d4b891d2d539a20f67c23e80430d8
Surya Hebbar <suryahebbar007@gmail.com>
2020-06-25 13:45:42 +0530

    Fixed the Single Quote bug

--- PHP_Scripts/filelink.php
+++ PHP_Scripts/filelink.php
@@ -7,14 +7,14 @@
 		echo "<p align='center' style='color:white;'>That directory does not exist! Opening default directory...</p>";
 	}
 	$files= array_diff(scandir("$path"),array("."));
-	shell_exec('rm -rf ../temp/* ../temp/.*');
+	shell_exec('rm -rf ../temp/* ../temp/*.*');
 	foreach( $files as $file){
 		if(is_dir("$path/$file")){
-			echo "<div class='linkcontainer'><input class='checkbox' type='checkbox' name='files' value='$path/$file'/><span class='dir' onclick='path=\"$path/$file\";reloadFileLinks();if(checkbox_flag){toogleAll();}'>$file</span></div>";
+			echo "<div class='linkcontainer'><input class='checkbox' type='checkbox' name='files' value=\"$path/$file\"/><span class='dir' onclick='path=\"$path/$file\";reloadFileLinks();if(checkbox_flag){toogleAll();}'>$file</span></div>";
 		}
 		else{
-			shell_exec("ln -s '$path/$file' '../temp/$file'");
-			echo "<div class='linkcontainer'><input class='checkbox' type='checkbox' name='files' value='$path/$file'/><a class='FileLink' download='$file' href='./temp/$file'>$file</a></div>";
+			shell_exec("ln -s \"$path/$file\" \"../temp/$file\"");
+			echo "<div class='linkcontainer'><input class='checkbox' type='checkbox' name='files' value=\"$path/$file\"/><a class='FileLink' download=\"$file\" href=\"./temp/$file\">$file</a></div>";
 		}
 	}
 ?>
--- PHP_Scripts/upload.php
+++ PHP_Scripts/upload.php
@@ -1,5 +1,7 @@
 <?php
-	$path = "/var/www/share/files_for_share";
+//	$path = "/var/www/share/files_for_share";
+	$path = "/mnt/Windows1/Share";
+//	$path = "/mnt/Linux1/Share";
 	if(!empty($_FILES['file'])){
 		move_uploaded_file($_FILES['file']['tmp_name'],"$path/".$_FILES['file']['name']);
 	}
